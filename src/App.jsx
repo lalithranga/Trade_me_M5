69089a4aa97cc164f302e433e08ad2fb257d8003
Autousername <cameron.belcher@developersinstitute.co.nz>
2025-04-04 17:02:26 +1300

    returned App.jsx how it was without the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
-      <Footer />
     </>
   );
 }
