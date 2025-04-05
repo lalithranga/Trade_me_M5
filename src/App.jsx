0ea7456d88d412023726b5d1a5396056141806f0
lalithranga <wkdlkumara@gmail.com>
2025-04-05 17:40:57 +1300

    Navigation_HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./component/LandingPage";
+import HomePage from "./component/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        {/* You can add more routes here */}
+        <Route path="/" element={<LandingPage />} />
+        {/* Click LandingPage search button */}
+        <Route path="/search" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
